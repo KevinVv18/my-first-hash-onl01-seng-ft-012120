8267991d2fad7bb07f1667059b78b88b16305d8f
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-29 01:05:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,12 @@
 def my_hash
- first_haash={ "name" => "Kevin", "age" => "18" } # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ my_self= { "name" => "Kevin", "age" => "18" } # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
 end
 
 
 def shipping_manifest
-the_manifest= { "whale  # set a variable called `the_manifest`, equal to a hash
+the_manifest= { "whale bone corsets" => "5"  # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 
