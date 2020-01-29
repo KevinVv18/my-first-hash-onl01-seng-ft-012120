f1873c0516fa56f4a32ecefc5fd6b3c7a48dc7e2
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-29 01:09:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,24 +1,26 @@
 def my_hash
- my_self= { "name" => "Kevin", "age" => "18" } # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
   
+ my_self= { "name" => "Kevin", "age" => "18" } # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+ 
 end
 
 
 def shipping_manifest
-  
- the_manifest= {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}  
+
+ the_manifest= {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+ 
 end
 
 def retrieval
+  
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+                 
+    shipping_manifest["oil paintings"]
+    
 end
 
 def adding
@@ -27,14 +29,14 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+    shipping_manifest                    # add 2 muskets to the shipping_manifest hash below
 
 
 
-  # add 4 gun powder to the shipping_manifest hash below
+                      # add 4 gun powder to the shipping_manifest hash below
 
 
 
-  # return the shipping_manifest hash below
+                      # return the shipping_manifest hash below
 
 end
